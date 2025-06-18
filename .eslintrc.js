b2807601c222594c8ed745ce14ec7385a12108e9
Chisom Ogbuanu <hi@chainsync.store>
2025-06-18 22:10:15 +0100

    Fix Vercel build: switch to CRACO, relax fullySpecified, disable ESLint in CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native-community/eslint-config',
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     requireConfigFile: false,
   },
---
+++ craco.config.js
@@ -0,0 +1,21 @@
+// craco.config.js
+// Customize CRA webpack to relax fullySpecified ESM import requirement for @react-navigation/native
+
+module.exports = {
+  eslint: {
+    enable: false, // Skip ESLint in create-react-app build
+  },
+  webpack: {
+    configure: (webpackConfig) => {
+      // Disable the fullySpecified check for @react-navigation/native
+      // Disable `fullySpecified` for *all* JS imports so packages that omit extensions (e.g. @react-navigation/*) resolve correctly.
+      webpackConfig.module.rules.push({
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      });
+      return webpackConfig;
+    },
+  },
+};
