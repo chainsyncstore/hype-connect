1df20fc8b2ecb6b32e48a247b21fc4008b981bd0
Chisom Ogbuanu <hi@chainsync.store>
2025-06-18 10:38:49 +0100

    Normalize line endings to LF

--- App.tsx
+++ App.tsx
@@ -5,24 +5,38 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
+import { useColorScheme } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './src/WelcomeScreen.jsx';
+import SignUpScreen from './src/SignUpScreen.jsx';
+import LoginScreen from './src/LoginScreen.jsx';
+import InterestsScreen from './src/InterestsScreen.jsx';
+import SuccessScreen from './src/SuccessScreen.jsx';
+import MainContentScreen from './src/MainContentScreen.jsx';
+import PostDetailScreen from './src/PostDetailScreen.jsx';
+import CommentSectionScreen from './src/CommentSectionScreen.jsx';
+
+const Stack = createStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Interests" component={InterestsScreen} />
+        <Stack.Screen name="Success" component={SuccessScreen} />
+        <Stack.Screen name="MainContent" component={MainContentScreen} />
+        <Stack.Screen name="PostDetail" component={PostDetailScreen} />
+        <Stack.Screen name="CommentSection" component={CommentSectionScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
